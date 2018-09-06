800f2f96fc430fb65941ad405a37379a8ca21e69
mauro <mneagoe@agn.gov.ar>
2018-09-06 12:20:14 -0300

    rutas creadas

--- heroes.js
+++ heroes.js
@@ -1,4 +1,4 @@
-const heroes = [
+let heroes = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
@@ -15,10 +15,40 @@ const heroes = [
 
 
 function getHeroes(req, res, next) {
-  res.json(heroes)
+  res.json(heroes);
+}
+
+function getHero(req, res, next) {
+  const heroId = parseInt(req.params.id);
+  const hero = heroes.filter(hero => hero.id === heroId)[0];
+  res.json(hero);
+}
+
+function addHero(req, res, next) {
+  let hero = req.body;
+  hero.id = (heroes[heroes.length - 1].id) + 1;
+  heroes.push(hero);
+  res.json(hero);
+}
+
+function updateHero(req, res, next) {
+  const heroData = req.body;
+  heroes.filter(hero => hero.id === heroData.id)
+    .map(hero => hero.name = heroData.name);
+
+  res.json(heroData);
+}
+
+function deleteHero(req, res, next) {
+  const heroId = parseInt(req.params.id);
+  heroes = heroes.filter(hero => hero.id !== heroId);
 }
 
 
 module.exports = {
-  getHeroes: getHeroes
+  getHeroes: getHeroes,
+  getHero: getHero,
+  addHero: addHero,
+  updateHero: updateHero,
+  deleteHero: deleteHero
 }
--- routes/heroes.js
+++ routes/heroes.js
@@ -4,5 +4,9 @@ var db = require('../heroes');
 
 /* GET home page. */
 router.get('/', db.getHeroes);
+router.get('/:id', db.getHero);
+router.post('/', db.addHero);
+router.put('/', db.updateHero);
+router.delete('/:id', db.deleteHero);
 
 module.exports = router;
